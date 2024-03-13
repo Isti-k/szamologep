43787aca8a361248bf936a62c1f021367a1afb6a
Isti-k <kiscsere03@gmail.com>
2024-03-13 13:46:17 +0100

    szamologep

---
+++ gombok.js
@@ -0,0 +1,9 @@
+export function gombok(){
+   
+    let txt = " ";
+    for (let i = 0; i < 10; i++) {
+       txt +=  `<button class="gombok">${i}</button>`;
+      }
+    return txt
+}
+
---
+++ main.js
@@ -0,0 +1,16 @@
+import { gombok } from "./gombok.js";
+
+const szamgomb = document.getElementsByClassName("szamok")[0];
+szamgomb.innerHTML += gombok();
+const szamokgomb = document.querySelectorAll(".gombok")
+
+for (let index = 0; index < szamokgomb.length; index++) {
+    szamgomb[index].addEventListener("click", katt);
+}
+
+const kijelzes = document.getElementsByClassName("kifejezes")[0];
+
+function katt(event){
+    kijelzes.innerHTML += event.target.innerHTML
+}
+
